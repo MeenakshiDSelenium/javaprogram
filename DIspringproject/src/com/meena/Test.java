44bdf728ab6a980f1b065c3e6dcac5aa8d4d4c47
hp <hp@user>
2019-01-27 17:36:23 +0530

    6commit

---
+++ DIspringproject/src/com/meena/Test.java
@@ -0,0 +1,22 @@
+package com.meena;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Resource resource=new ClassPathResource("ApplicationContext.xml");
+		BeanFactory factory=new XmlBeanFactory(resource);
+		
+		Student s=(Student)factory.getBean("s");
+		s.m1();
+		
+				
+
+	}
+
+}
