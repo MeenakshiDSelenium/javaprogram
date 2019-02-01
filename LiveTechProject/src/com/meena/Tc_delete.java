e8b29f6a6dab484fefa57ae0ca96ecb4bcfa8950
hp <hp@user>
2019-02-01 16:53:29 +0530

    update

--- LiveTechProject/src/com/meena/Tc_delete.java
+++ LiveTechProject/src/com/meena/Tc_delete.java
@@ -2,14 +2,20 @@ package com.meena;
 
 
 
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class Tc_delete {
+
+	//private static String value;
+
+	
 
 	public static void main(String[] args)throws Exception {
 		System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\chromedriver.exe");
@@ -23,27 +29,47 @@ public class Tc_delete {
 		System.out.println("passWord entered");
 		driver.findElement(By.xpath("//table[@id='Table_01']/tbody/tr/td[2]/table/tbody/tr[4]/td/input")).click();
 		Thread.sleep(3000);
-		driver.switchTo().frame(0);
-		//comm
-		Select drpdwn=new Select(driver.findElement(By.name("loc_code")));
-		drpdwn.selectByIndex(1);
-		System.out.println("selected empID");
-		driver.findElement(By.name("loc_name")).sendKeys("0006");
-		System.out.println("search emp id");
-		driver.findElement(By.xpath("//input[@type='button']")).click();
-		System.out.println("clicked search button");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//*[@id=\"standardView\"]/table/tbody/tr/td[1]/input")).click();
-		System.out.println("clicked checkbox");
-		driver.findElement(By.xpath("//*[@id=\"standardView\"]/div[3]/div[1]/input[2]")).click();
-		System.out.println("clicked delete button");
-		driver.switchTo().defaultContent();
-		driver.findElement(By.linkText("Logout")).click();
-		System.out.println("logiut completed");
-		driver.close();
-	
+        driver.switchTo().frame(0);
+        
+        WebElement table=driver.findElement(By.xpath("//table[@class='data-table']"));
+		List<WebElement>rows=table.findElements(By.tagName("tr"));
+		System.out.println(rows.size());
+		for(int i=3;i<rows.size();i++) {
+			driver.findElement(By.xpath("//*[@id=\'standardView\']/table/tbody/tr["+i+"]/td[1]/input")).click();
+			
+			
+		/*List<WebElement> tbody=driver.findElements(By.xpath("//input[@type='checkbox']"));
+		System.out.println(tbody.size());
+		for(int i=0;i<tbody.size()-2;i++) {
+			tbody.get(i).click();*/
+			
+			
+			//driver.findElement(By.xpath("//*[@id='standardView']/div[3]/div[1]/input[2]")).click();
+			//System.out.println("deleted successfully");
+			
+		}}}	
+			
+       /* WebElement oCheckBox = driver.findElement(By.cssSelector("input[value='0002']"));
+        oCheckBox.click();*/
+        
 		
+		//driver.findElement(By.xpath("//*[@id='standardView']/div[3]/div[1]/input[2]")).click();
+		//System.out.println("deleted successfully");
+        
+        //using this selected all checkboxs
+        /*List<WebElement> ddc=driver.findElements(By.xpath("//input[@type='checkbox']"));
+        
+        System.out.println(ddc.size());
+        for(WebElement el:ddc) {
+        	if(!el.isSelected()) {
+        	el.click();
+        }
+        }*/
+        
+	
 
-	}
+	
+	
+	
 
-}
+
