f7a8a18c53a9778d72b96d458102befeb6302212
hp <hp@user>
2019-03-22 16:30:32 +0530

    commit

---
+++ ProjectAccessmodifier/src/com/package1/A.java
@@ -0,0 +1,13 @@
+package com.package1;
+
+ public class A {
+	 public int x=15;
+	
+
+	public static void main(String[] args) {
+		A a=new A();
+		System.out.println(a.x);
+
+	}
+
+}
---
+++ ProjectAccessmodifier/src/com/package2/D.java
@@ -0,0 +1,12 @@
+package com.package2;
+import com.package1.*;
+
+public class D extends A {
+
+	public static void main(String[] args) {
+		D d=new D();
+		System.out.println(d.x);
+
+	}
+
+}
